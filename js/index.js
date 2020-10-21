a781bded92489fd4328025ae5476062c5b6647b0
JoniMujica <jonimujica3@hotmail.com>
2020-10-21 17:39:45 -0300

    cambio el tipo de carga de body por window y elimino sus referencias

--- js/index.js
+++ js/index.js
@@ -1,23 +1,29 @@
 //aca importo el array de objetos de data.js (esto para manejarlo como si fuera una db o API)
-import datos from './data';
+import datos from './data.js';
+var cartas = document.getElementsByClassName("cards");
+var image = document.getElementsByTagName("img");
+var totuloprod = document.getElementsByTagName("h2");
+var precio = document.getElementsByClassName("price");
+var descripcion = document.getElementsByClassName("desc");
 
-var cuerpo = document.getElementById("cuerpo");
-
+/*
+for (var i = 0; i < cartas.length; i++) {
+    image[i].src = datos[i].src;
+    console.log(image[i]);
+    console.log(datos[i].src)
+} */
 
 
 function cargarElementos(){
-    var cartas = document.getElementsByClassName("cards");
-    var image = document.getElementsByTagName("img");
-    var totuloprod = document.getElementsByTagName("h2");
-    var precio = document.getElementsByClassName("price");
-    var descripcion = document.getElementsByClassName("desc");
-
+    
     for (var i = 0; i < cartas.length; i++) {
         image[i].src = datos[i].src;
+        console.log(image[i]);
+        console.log(datos[i].src)
     }
 
 }
 
 
 //cuando cargue el HTML, manda a ejecutar la funcion 'cargarElementos'
-cuerpo.addEventListener('load',cargarElementos());
+window.addEventListener('load',cargarElementos);
