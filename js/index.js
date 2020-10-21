0a35c6d2e379ae7da551b2eb45b8ad08df7a9b3a
JoniMujica <jonimujica3@hotmail.com>
2020-10-21 16:52:21 -0300

    creo una funcion que cargara los elementos de productos al HTML

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 //aca importo el array de objetos de data.js (esto para manejarlo como si fuera una db o API)
 import datos from './data';
 
+var cuerpo = document.getElementById("cuerpo");
+
+
+
+function cargarElementos(){
+
+}
+
+
+//cuando cargue el HTML, manda a ejecutar la funcion 'cargarElementos'
+cuerpo.addEventListener('load',cargarElementos());
