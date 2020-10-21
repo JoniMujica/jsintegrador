8cafb57bb4ca7060038393c7c3ea962874fd5e2e
JoniMujica <jonimujica3@hotmail.com>
2020-10-21 17:13:30 -0300

    subo las imagenes y las vinculo con el DOM atraves del array de objetos

--- js/index.js
+++ js/index.js
@@ -2,14 +2,19 @@
 import datos from './data';
 
 var cuerpo = document.getElementById("cuerpo");
-var cartas = document.getElementsByClassName("cards");
-var image = document.getElementsByTagName("img");
-var totuloprod = document.getElementsByTagName("h2");
-var precio = document.getElementsByClassName("price");
-var descripcion = document.getElementsByClassName("desc");
+
 
 
 function cargarElementos(){
+    var cartas = document.getElementsByClassName("cards");
+    var image = document.getElementsByTagName("img");
+    var totuloprod = document.getElementsByTagName("h2");
+    var precio = document.getElementsByClassName("price");
+    var descripcion = document.getElementsByClassName("desc");
+
+    for (var i = 0; i < cartas.length; i++) {
+        image[i].src = datos[i].src;
+    }
 
 }
 
