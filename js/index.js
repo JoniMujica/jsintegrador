7031ba7e81e2dcc7ac2a72ae1fdd529d1d577b63
JoniMujica <jonimujica3@hotmail.com>
2020-10-22 17:38:11 -0300

    creo las cartas de forma generica y emito los elementos condicionando si hay elmentos o no en la db (array de objetos que emularia info de una db)

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 //aca importo el array de objetos de data.js (esto para manejarlo como si fuera una db o API)
 import datos from './data.js';
 
-
+var tablaro = document.getElementById("tablaP");
+/*
 function cargarElementos(){
     var cartas = document.getElementsByClassName("cards");
     var image = document.getElementsByTagName("img");
@@ -9,7 +10,7 @@ function cargarElementos(){
     var precio = document.getElementsByClassName("price");
     var descripcion = document.getElementsByClassName("desc");
 
-    for (var i = 0; i < cartas.length; i++) {
+    for (var i = 0; i < datos.length; i++) {
         image[i].src = datos[i].src;
         image[i].alt = datos[i].title;
         image[i].title = datos[i].title;
@@ -17,8 +18,43 @@ function cargarElementos(){
         descripcion[i].innerText = datos[i].descripcion;
         precio[i].innerText = "$" + datos[i].price
     }
+}  */
+
+
+
+var espacioTAB = document.createElement('div')
+
+function cargarElementos(){
+    
+
+    if ( validarDatos() == true) {
+        for (var i = 0; i < datos.length; i++) {
+        const contenido = `
+            <div class="cards">
+            <img src=${datos[i].src} >
+            <h2>${datos[i].title}</h2>
+            <p class="price">${datos[i].price}</p>
+            <p class="desc">${datos[i].descripcion}</p>
+            <button class="btnAgregar">Agregar al Carrito</button>`
+            tablaro.innerHTML += contenido;
+        }
+    }else{
+        // si no hay elementos en el array de objetos, imprime en pantalla esto
+        tablaro.innerHTML = "<p>No se encontraron elementos en la base de datos!!</p>";
+    }
+
+} 
+
+function validarDatos(){
+    return datos.length != 0 ? true : false
 }
 
+function createElement(str)
+{
+  var div = document.createElement('div');
+  div.innerHTML = str;
+  return div.childNodes;
+}
 
 //cuando cargue el HTML, manda a ejecutar la funcion 'cargarElementos'
 window.addEventListener('load',cargarElementos);
